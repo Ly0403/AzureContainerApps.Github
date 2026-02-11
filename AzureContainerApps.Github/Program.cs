19c7c59d3353bf78bc96841f91210b5cbe733130
admin <admin@lytraining2016.tk>
2026-02-11 20:15:24 +0300

    feat: added v2 endpoint

--- AzureContainerApps.Github/Program.cs
+++ AzureContainerApps.Github/Program.cs
@@ -5,5 +5,6 @@ var app = builder.Build();
 app.UseHttpsRedirection();
 
 app.MapGet("/", () => "Deployed to Azure Container Apps");
+app.MapGet("/v2", () => "Deployed to Azure Container Apps v2");
 
 app.Run();
